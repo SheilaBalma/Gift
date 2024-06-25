6727eed9c281abca2123e6a2fec065bf90a55fe0
SheilaBalma <sheiidbalmacedaa@gmail.com>
2024-06-24 22:25:41 -0300

    Intento1

---
+++ Regl.js
@@ -0,0 +1,11 @@
+// modal.js
+
+var btn = document.getElementById("openAlertBtn");
+
+// Función para mostrar la alerta y redirigir al hacer clic en el botón
+btn.onclick = function() {
+    // Mostrar alerta
+    alert("Espero que te guste, Te amo!");
+    // Redirigir a un enlace
+    window.location.href = "https://docs.google.com/document/d/1M8NTKKuDEJJKvZI7tQMAuSobX1f-1g9X_MTYvlLnIYc/edit?usp=sharing"; // Reemplaza con el enlace que desees
+}
